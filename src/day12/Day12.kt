eb127d65c20e99e1be18413251e88f6628277abd
Matthias Achtelik <matthias.achtelik@mail.schwarz>
2023-12-13 01:01:35 +0100

    Day12

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -59,35 +59,18 @@ class Day12Solution1 {
 
 class Day12Solution2 {
 
-    data class Input(
-        val extended: Int,
-        val data: String,
-        val dataExtended: String,
-        val validation: String,
-        val validationExtended: String,
-        val possibilities: List<String>
-    )
+    data class Input(val input: String, val validation: String, val possibilities: List<String>)
 
     fun solve(input: List<String>): Long {
-        val input = createInput(input)
-        return input.sumOf { it.possibilities.size }.toLong()
+        val inputData = createInput(input)
+        return inputData.sumOf { it.possibilities.size }.toLong()
     }
 
     fun createInput(input: List<String>): List<Input> {
         return input.parallelStream().map {
-            System.out.println(Thread.currentThread().getName())
-            val data = it.split(" ")[0]
-            val validation = it.split(" ")[1]
-            Input(
-                1,
-                data,
-                data.extend('?', '.'),
-                validation,
-                validation.extend(',', ','), replaceQuestionMark(
-                    data,
-                    validation
-                )
-            )
+            val data = it.split(" ")[0].repeat(5, "?")
+            val validation = it.split(" ")[1].repeat(5, ",")
+            Input(data, validation, replaceQuestionMark(data, validation))
         }.toList()
     }
 
@@ -116,13 +99,8 @@ class Day12Solution2 {
         }
     }
 
-    private fun String.extend(addition: Char, seperator: Char): String {
-        val extension = this.substring(0, this.indexOfFirst { it == seperator })
-        val new = "$this$addition$extension"
-        return new
-    }
-
-    private fun String.extend(addition: Char): String {
-        return "$this$addition$this"
+    private fun String.repeat(n: Int, separator: String): String {
+        val new = "$this$separator".repeat(n)
+        return new.substring(0, new.lastIndexOf(separator))
     }
 }
